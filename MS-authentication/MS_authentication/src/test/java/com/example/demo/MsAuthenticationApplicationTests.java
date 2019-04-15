40c20c4029bfde55636e59e484c1244726632745
aspahic5 <aspahic5@etf.unsa.ba>
2019-04-15 23:39:37 +0200

    unit testovi za sinhronu komunikaciju

--- MS-authentication/MS_authentication/src/test/java/com/example/demo/MsAuthenticationApplicationTests.java
+++ MS-authentication/MS_authentication/src/test/java/com/example/demo/MsAuthenticationApplicationTests.java
@@ -1,16 +1,71 @@
 package com.example.demo;
 
+
+
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MsAuthenticationApplicationTests {
 
+	
 	@Test
-	public void contextLoads() {
-	}
+	public void testPravilnaAutentifikacija() throws Exception
+	{
+		
+		RestTemplate restTemplate = new RestTemplate();
+
+		
+		HttpHeaders headers = new HttpHeaders();
+    	headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    	MultiValueMap<String, Object> body= new LinkedMultiValueMap<>();
+    	body.add("username","korisnik2-username");
+    	body.add("password", "korisnik2-password");
+    	
+    	HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+    	 
+    	ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/provjeri", requestEntity, String.class);
+    	JSONObject o=new JSONObject(response.getBody().toString());
+    	
+    	Assert.assertEquals(4, o.getInt("id"));
+    	
+}
+	
+	@Test
+	public void testPogrenaAutentifikacija() throws Exception
+	{
+		
+		RestTemplate restTemplate = new RestTemplate();
+
+		
+		HttpHeaders headers = new HttpHeaders();
+    	headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    	MultiValueMap<String, Object> body= new LinkedMultiValueMap<>();
+    	body.add("username","pogresan");
+    	body.add("password", "pogresan");
+    	
+    	HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+    	 
+    	ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/provjeri", requestEntity, String.class);
+    	JSONObject o=new JSONObject(response.getBody().toString());
+    	
+    	Assert.assertEquals(-1, o.getInt("id"));
+    	
+		
+		}
 
 }
